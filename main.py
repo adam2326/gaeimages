f606c4b3fa16d1a91031f1b3c294496427735d00
adam2326 <adampilz.2326@gmail.com>
2016-11-18 21:04:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-
+import logging
 from flask import Flask, request, url_for, render_template
+from google.appengine.api import users
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def landing_page():
-	return render_template('landing_page.html')
 
 @app.route('/')
 def home_page():

1b6a29c8997b0c74e534ad5370df1998760c0e8b
adam2326 <adampilz.2326@gmail.com>
2016-11-21 22:02:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def home_page():
     	nickname = 'Unknown User'
         login_logout_url = users.create_login_url('/')
 
-    return render_template('landing_page.html', nickname = nickname, login_logout_url = login_logout_url)
+    return render_template('landing_page.html', user = user, nickname = nickname, login_logout_url = login_logout_url)
 
 
 @app.errorhandler(500)

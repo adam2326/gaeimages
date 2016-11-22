963e77603af261bb7a0f2eb7de8d669ed9d2b231
adam2326 <adampilz.2326@gmail.com>
2016-11-22 11:33:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 # some metadata to make available to all sites and functions
 app.vars = {}
-app['APPLICATION_ID'] = os.environ['APPLICATION_ID']
-app['CURRENT_VERSION_ID'] = os.environ['CURRENT_VERSION_ID']
-app['SERVER_SOFTWARE'] = os.environ['SERVER_SOFTWARE']
-app['AUTH_DOMAIN'] = None
+#app['APPLICATION_ID'] = os.environ['APPLICATION_ID']
+#app['CURRENT_VERSION_ID'] = os.environ['CURRENT_VERSION_ID']
+#app['SERVER_SOFTWARE'] = os.environ['SERVER_SOFTWARE']
+#app['AUTH_DOMAIN'] = os.environ['AUTH_DOMAIN']
 
 
 @app.route('/')
@@ -21,12 +21,11 @@ def home_page():
     if user:
         nickname = user.nickname()
         login_logout_url = users.create_logout_url('/')
-        app['AUTH_DOMAIN'] = os.environ['AUTH_DOMAIN']
     else:
     	nickname = 'Unknown User'
         login_logout_url = users.create_login_url('/')
 
-    return render_template('landing_page.html', user = user, nickname = nickname, login_logout_url = login_logout_url, app_id = app['APPLICATION_ID'], version_id = app['CURRENT_VERSION_ID'], auth_domain = app['AUTH_DOMAIN'], server_software = app['SERVER_SOFTWARE'])
+    return render_template('landing_page.html', user = user, nickname = nickname, login_logout_url = login_logout_url, app_id = os.environ['APPLICATION_ID'], version_id = os.environ['CURRENT_VERSION_ID'], auth_domain = os.environ['AUTH_DOMAIN'], server_software = os.environ['SERVER_SOFTWARE'])
 
 
 @app.errorhandler(500)

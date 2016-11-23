cca9a90e2b4f42d49ad3789074808e618cff4dcc
adam2326 <adampilz.2326@gmail.com>
2016-11-23 16:53:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def home_page():
         #url = 'http://www.google.com/humans.txt'
         result = urlfetch.fetch(url, validate_certificate=True, follow_redirects=False)
         if result.status_code == 200:
-            api_result = json.loads(result.content)
+            #api_result = json.loads(result.content)
             # manipulate the response
             api_result = str(type(result.content))
         else:
